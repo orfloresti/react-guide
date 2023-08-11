d535e25ebcee1861a3ca97b3420ca7e104ce4c40
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2023-08-11 17:33:49 -0600

    Add business logic and validation

--- 09-calculator/src/App.jsx
+++ 09-calculator/src/App.jsx
@@ -1,34 +1,75 @@
+import { useState } from 'react';
 import {Formik, Form} from 'formik'
+import * as Yup from 'yup';
 import Input from './components/Input';
 import Button from './components/Button';
 import Container from './components/Container';
 import Section from './components/Section';
+import Balance from './components/Balance';
+
+const compoundInterest = (deposit, contribution, years, rate) => {
+
+  let total = deposit;
+  for( let i = 0; i < years; i++) {
+    total = (total + contribution) * (rate + 1); 
+  }
+
+  return Math.round(total);
+}
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style:'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2, 
+})
 
 function App() {
-  const handleSubmit = () => {}
+  const [balance, setBalance] = useState('');
+  const handleSubmit = ({deposit, contribution, years, rate}) => {
+    const val = compoundInterest(Number(deposit), Number(contribution), Number(years),Number(rate));
+    setBalance(formatter.format(val));
+  }
   return (
     <Container>
       <Section>
         <Formik
           initialValues={{
-            deposit: '',
-            contribution: '',
-            years: '',
-            rate: ''
+            deposit: "",
+            contribution: "",
+            years: "",
+            rate: "",
           }}
           onSubmit={handleSubmit}
+          validationSchema={Yup.object({
+            deposit: Yup.number()
+              .required("Obligatorio")
+              .typeError("Debe ser un número"),
+            contribution: Yup.number()
+              .required("Obligatorio")
+              .typeError("Debe ser un número"),
+            years: Yup.number()
+              .required("Obligatorio")
+              .typeError("Debe ser un número"),
+            rate: Yup.number()
+              .required("Obligatorio")
+              .typeError("Debe ser un número")
+              .min(0, "El valor minimo es 0")
+              .max(1, "El valor maximo es 1"),
+          })}
         >
           <Form>
-            <Input name="deposit" label="Deposito inicial"/>
-            <Input name="contribution" label="Contribución anual"/>
-            <Input name="years" label="Años"/>
-            <Input name="rate" label="Interés estimado"/>
-            <Button>Calcular</Button>
+            <Input name="deposit" label="Deposito inicial" />
+            <Input name="contribution" label="Contribución anual" />
+            <Input name="years" label="Años" />
+            <Input name="rate" label="Interés estimado" />
+            <Button type="submit">Calcular</Button>
           </Form>
         </Formik>
+        {balance !== "" ? <Balance>balance final: {balance}</Balance> : null}
       </Section>
     </Container>
-  )
+  );
 }
 
 export default App
