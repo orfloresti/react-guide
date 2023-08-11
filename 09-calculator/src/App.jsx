cad891ef7d9273d5da9ea74fc5df7520acd7dc33
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2023-08-11 17:10:15 -0600

    Add template of the project

--- 09-calculator/src/App.jsx
+++ 09-calculator/src/App.jsx
@@ -1,17 +1,32 @@
-import styled from 'styled-components';
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 100%;
-  align-items: center;
-`
+import {Formik, Form} from 'formik'
+import Input from './components/Input';
+import Button from './components/Button';
+import Container from './components/Container';
+import Section from './components/Section';
 
 function App() {
-
+  const handleSubmit = () => {}
   return (
     <Container>
-      lala
+      <Section>
+        <Formik
+          initialValues={{
+            deposit: '',
+            contribution: '',
+            years: '',
+            rate: ''
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <Input name="deposit" label="Deposito inicial"/>
+            <Input name="contribution" label="Contribución anual"/>
+            <Input name="years" label="Años"/>
+            <Input name="rate" label="Interés estimado"/>
+            <Button>Calcular</Button>
+          </Form>
+        </Formik>
+      </Section>
     </Container>
   )
 }
