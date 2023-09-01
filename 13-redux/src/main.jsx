4e08893a81508e4d16f7717297f136babbc4d671
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2023-08-31 18:16:25 -0600

    Add dependencie for redux

--- 13-redux/src/main.jsx
+++ 13-redux/src/main.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { legacy_createStore as createStore } from 'redux'
+
+const store = createStore((state = 0, action) => {
+  switch(action.type) {
+    case 'action':
+      return action.payload;
+  }
+  return state;
+})
+
+store.dispatch({type: 'action', payload: 2});
+console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
